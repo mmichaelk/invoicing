f001e74085900a3f0d52b25b7d384d6b42644ef3
MichaelK <michael.2212.k@gmail.com>
2019-08-06 15:05:48 +0300

    Updates

---
+++ precache-manifest.0256f017da0ce061801f46823f082b3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9a534fbf4314702e0fc959620bf100a",
+    "url": "/invoicing/index.html"
+  },
+  {
+    "revision": "5c5811ff041c3ec2da06",
+    "url": "/invoicing/static/css/2.693a33c4.chunk.css"
+  },
+  {
+    "revision": "c45c78f0039ac87f5288",
+    "url": "/invoicing/static/css/main.a9dc1067.chunk.css"
+  },
+  {
+    "revision": "5c5811ff041c3ec2da06",
+    "url": "/invoicing/static/js/2.0e2daa91.chunk.js"
+  },
+  {
+    "revision": "c45c78f0039ac87f5288",
+    "url": "/invoicing/static/js/main.1dba4bd2.chunk.js"
+  },
+  {
+    "revision": "4108d849a4a16789addf",
+    "url": "/invoicing/static/js/runtime~main.7de17f9f.js"
+  },
+  {
+    "revision": "2fcb85084d8306f94beb73f6a8760b99",
+    "url": "/invoicing/static/media/logo.2fcb8508.png"
+  }
+]);
