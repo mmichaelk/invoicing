8512560a4a9d1adfca2d9c4a0664306e0d05a39a
Michael Kozlov <mkozlov@neoflex.ru>
2019-08-15 16:49:00 +0300

    chore: deploy

---
+++ precache-manifest.0eaa7a1b916cbd68df7cb25dcd769ade.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5073e0f29e96e9af6cf42a42fd751002",
+    "url": "/invoicing/index.html"
+  },
+  {
+    "revision": "816a4748a22e3ae18f20",
+    "url": "/invoicing/static/css/2.6fc15695.chunk.css"
+  },
+  {
+    "revision": "643ba923a332878f55f8",
+    "url": "/invoicing/static/css/main.4b3a6c92.chunk.css"
+  },
+  {
+    "revision": "816a4748a22e3ae18f20",
+    "url": "/invoicing/static/js/2.36daee5d.chunk.js"
+  },
+  {
+    "revision": "643ba923a332878f55f8",
+    "url": "/invoicing/static/js/main.0f2fb16e.chunk.js"
+  },
+  {
+    "revision": "4108d849a4a16789addf",
+    "url": "/invoicing/static/js/runtime~main.7de17f9f.js"
+  },
+  {
+    "revision": "2fcb85084d8306f94beb73f6a8760b99",
+    "url": "/invoicing/static/media/logo.2fcb8508.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.16b5566c9bab03ff1d3a98b515186284.js"
+  "/invoicing/precache-manifest.0eaa7a1b916cbd68df7cb25dcd769ade.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/invoicing/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
